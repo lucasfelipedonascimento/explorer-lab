c84a25002fb7c12dede9dd51bdeabdcea2278292
Lucas Felipe <lucasfelipe3000@gmail.com>
2022-10-18 16:27:58 -0300

    adicionando as logo-marca dos cartões

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import "./css/index.css"
 
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path");
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path");
-const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img");
+const ccIcon = document.querySelector(".cc-logo span:nth-child(1) img");
+const ccLogo = document.querySelector(".cc-logo span:nth-child(3) img");
+
 
 function setCardType(type) {
     const colors = {
-    visa: ["#2D57F2", "#436D99"],
+    visa: ["#013BAD", "#FFB800"],
     visaPremium: ["#080064", "#0F4B00"],
     nubank: ["#2A005F", "#8F29DF"],
     mastercard: ["#C69347", "#DF6F29"],
@@ -16,6 +18,7 @@ function setCardType(type) {
 
   ccBgColor01.setAttribute("fill", colors[type][0]);
   ccBgColor02.setAttribute("fill", colors[type][1]);
+  ccIcon.setAttribute("src", `cc-marca-${type}.svg`)
   ccLogo.setAttribute("src", `cc-${type}.svg`);
 }
 
