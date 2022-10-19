d17ad289e693baf7f704c0b44d0f34b59a1a0004
Lucas Felipe <lucasfelipe3000@gmail.com>
2022-10-19 09:58:10 -0300

    instalando o IMASK e utilizando-o no formulario

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import "./css/index.css"
+import IMask from 'imask'
 
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path");
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path");
@@ -12,7 +13,7 @@ function setCardType(type) {
     visaPremium: ["#080064", "#0F4B00"],
     nubank: ["#2A005F", "#8F29DF"],
     mastercard: ["#C69347", "#DF6F29"],
-    masterPremium: ["#FF9A03", "#A70000"],
+    elo: ["#002F8A", "#FF6200"],
     default: ["black", "gray"]
   }
 
@@ -23,3 +24,68 @@ function setCardType(type) {
 }
 
 globalThis.setCardType = setCardType;
+
+// card number
+const cardNumber = document.querySelector("#card-number")
+const cardNumberPattern = {
+  mask: [
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^4\d{0-15}/,
+      cardType: "visa",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
+      cardType: "mastercard",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      cardType: "default",
+    },
+  ],
+  dispatch: function() {
+    const number = (dynamicMasked.value + appended).replace(/\D/g,'');
+    const foundMask = dynamicMasked.compiledMasks.find(function(item) {
+      return number.match(item.regex)
+    })
+
+    return foundMask;
+  },
+}
+const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
+
+// card holder
+const cardHolder = document.querySelector("#card-holder")
+const cardHolderPattern = {
+  mask: "aaaaa"
+}
+const cardHolderMasked = IMask(cardHolder, cardHolderPattern)
+
+// expiration date 
+const expirationDate = document.querySelector("#expiration-date")
+const expirationDatePattern = {
+  mask: "MM{/}YY",
+  blocks: {
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12
+    },
+
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2)
+    }
+  }
+}
+const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
+
+// security code
+const securityCode =  document.querySelector("#security-code")
+const securityCodePattern = {
+  mask: "000"
+}
+const securityCodeMasked = IMask(securityCode, securityCodePattern)
+
